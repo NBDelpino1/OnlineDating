8e7ef126fd0582de7fcfb5b81951827c21b1be47
Nicholas Delpino <NBDelpino4@gmail.com>
2017-01-30 23:49:18 -0500

    messing around with the layout, changed colors and adjusted container

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
+            $('views, body').animate({
                 scrollTop: $(hash).offset().top
             }, 900, function(){
 
